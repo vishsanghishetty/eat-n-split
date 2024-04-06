467da8095391666f4fc91af3daec86182fd6d7a8
vsanghishetty <vishali.kamenani@ymail.com>
2024-04-06 09:11:01 -0400

    adds initial list of friends

---
+++ src/initialList.js
@@ -0,0 +1,8 @@
+const list = [
+    { id: 1, name: 'John Doe', image: 'https://i.pravatar.cc/95?u=12345', balance: 10 },
+    { id: 2, name: 'Jane Doe', image: 'https://i.pravatar.cc/175?u=23345', balance: -20 },
+    { id: 3, name: 'Jim Doe', image: 'https://i.pravatar.cc/75?u=32345', balance: 45 },
+    { id: 4, name: 'Jill Doe', image: 'https://i.pravatar.cc/75?u=42345', balance: 0 },
+];
+
+export default list;
